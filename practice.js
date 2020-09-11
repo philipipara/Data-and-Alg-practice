568e225f0d5c5f67b2b3e33c5656bdd76c322217
Philip <philip.iparaguirre@gmail.com>
2020-09-11 09:31:40 -0700

    updated

--- practice.js
+++ practice.js
@@ -151,3 +151,51 @@ function invertBinaryTree(tree) {
       }
       return stack.length === 0;
   }
+
+
+//Write a function that takes in an n by m two dimensional array, that can be squared
+//shaped when n = m and returns one dimensional array of all the arrayss elements in zigag order
+
+  function zigzagTraverse(array) {
+    const height = array.length -1;
+      const width = array[0].length -1;
+      const result = [];
+      let row = 0;
+      let col = 0;
+      let goingDown = true;
+      while(!isOutOfBounds(row, col, height, width)){
+          result.push(array[row][col]);
+          if(goingDown) {
+              if(col === 0 || row === height){
+                  goingDown = false;
+                  if(row === height) {
+                      col++
+                  } else {
+                      row++
+                  } 
+              } else {
+                      row++;
+                      col--;
+                  }
+              } else {
+                  if(row === 0 || col === width) {
+                      goingDown = true;
+                      if(col === width) {
+                          row++
+                      } else {
+                          col++;
+                      }
+                  } else {
+                      row--;
+                      col++;
+                  }
+              }
+      }
+      return result;
+  }
+  
+  //helper function
+  function isOutOfBounds(row, col, height, width){
+      return row < 0 || row > height || col < 0 || col > width;
+  }
+  
