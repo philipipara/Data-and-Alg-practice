a605fbe1cb42cd6a37e89206df536d4597b35184
Philip <philip.iparaguirre@gmail.com>
2020-09-19 12:37:40 -0700

    another problem down

--- practice.js
+++ practice.js
@@ -198,4 +198,35 @@ function invertBinaryTree(tree) {
   function isOutOfBounds(row, col, height, width){
       return row < 0 || row > height || col < 0 || col > width;
   }
+
+
+//Given an array, find the three largest numbers in the array and return an array sorted with three largest numbers
+function findThreeLargestNumbers(array) {
+	const threeLargest = [null, null, null];
+	for(const num of array){
+		updateLargest(threeLargest, num);
+	}
+	return threeLargest;
+}
+
+
+function updateLargest(threeLargest, num) {
+	if(threeLargest[2] === null || num > threeLargest[2]) {
+		shiftAndUpdate(threeLargest, num, 2);
+	  } else if(threeLargest[1] === null || num > threeLargest[1]){
+			shiftAndUpdate(threeLargest, num, 1);
+		} else if(threeLargest[0] === null || num > threeLargest[0]){
+			shiftAndUpdate(threeLargest, num, 0);
+		}
+	}
+
+function shiftAndUpdate(array, num, idx) {
+	for(let i = 0; i <= idx; i++) {
+		if (i === idx) {
+			array[i] = num;
+		} else {
+			array[i] = array[i + 1];
+		}
+	}
+}
   
