750e48e9bdeb5b4a3abfd1da8d0d23d05d8bcab6
Philip <philip.iparaguirre@gmail.com>
2020-09-22 16:33:56 -0700

    insertion sort

--- practice.js
+++ practice.js
@@ -255,4 +255,22 @@ function bubbleSort(array) {
       array[j] = array[i];
       array[i] = temp;
   }
+
+  //use insertion sort
+
+  function insertionSort(array) {
+    for(let i = 1; i < array.length; i++){
+          let j = i;
+          while (j > 0 && array[j] < array[j -1]){
+              swap(j, j -1 , array);
+              j -=1;
+          }
+      }
+      return array
+  }
   
+  function swap(i, j, array){
+      const temp= array[j];
+      array[j] = array[i]
+      array[i] = temp;
+  }
