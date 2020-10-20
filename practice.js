40cb9a236469c58584e8eb79bd4273b4a962b706
Philip <philip.iparaguirre@gmail.com>
2020-10-20 07:43:57 -0700

    closest value binary tree

--- practice.js
+++ practice.js
@@ -340,3 +340,32 @@ function searchMatrix(matrix, target) {
       return seqI === sequence.length;
   }
   
+
+
+  //Using a binary search tree find the closest value given a target value. 
+  function findClosestValueInBst(tree, target) {
+    return findClosestHelper(tree, target, tree.value);
+  }
+  
+   function findClosestHelper(tree, target, closest) {
+       if(tree === null) return closest;
+       if(Math.abs(target - closest) > Math.abs(target - tree.value)) {
+           closest = tree.value;
+       }
+       if(target < tree.value) {
+           return findClosestHelper(tree.left, target, closest);
+       } else if (target > tree.value){
+           return findClosestHelper(tree.right, target, closest);
+       } else {
+           return closest;
+       }
+   }
+  // This is the class of the input tree. Do not edit.
+  class BST {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
